86e18368e3229db51b11b39fd828cccfbfed283b
jin5354 <xiaoyanjinx@gmail.com>
2017-08-22 14:39:20 +0800

    [feat] add concact notification

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @Filename: index.js
  * @Author: jin
  * @Email: xiaoyanjinx@gmail.com
- * @Last Modified time: 2017-08-22 10:18:42
+ * @Last Modified time: 2017-08-22 14:37:02
  */
 
 import express from 'express'
@@ -11,17 +11,7 @@ import multer from 'multer'
 import {Wechaty, Room, Contact} from 'wechaty'
 import QrcodeTerminal from 'qrcode-terminal'
 
-let app = express()
-let upload = multer()
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-app.post('/sendAlertMessage', upload.array(), function (req, res) {
-  console.log(req.body)
-  res.send('Hello World')
-})
-
-app.listen(9000)
+let concact
 
 Wechaty.instance() // Singleton
   .on('scan', (url, code) => {
@@ -36,6 +26,10 @@ Wechaty.instance() // Singleton
     Room.find({topic: /@zju$/}).then((room) => {
       console.log('find room @zju')
     })
+    Contact.find({name: 'jin'}).then((target) => {
+      console.log(JSON.stringify(target))
+      concact = target
+    })
   })
   .on('message', (message) => {
     const room    = message.room()
@@ -45,9 +39,23 @@ Wechaty.instance() // Singleton
     console.log(sender)
     console.log(content)
 
-    if(!message.self()) {
-      //room.say(JSON.stringify(sender.obj))
-      //room.say('content:' + content)
+    if(!message.self() && concact) {
+      concact.say(content)
     }
   })
   .init()
+
+let app = express()
+let upload = multer()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/sendAlertMessage', upload.array(), function (req, res) {
+  console.log(req.body)
+  res.send(req.body)
+  if(concact) {
+    concact.say(JSON.stringify(req.body))
+  }
+})
+
+app.listen(9000)
